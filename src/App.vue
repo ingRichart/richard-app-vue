bbaa6c71f503df5ecd6733312563e55d70631985
Ricardo Elizalde <ricardo.elizaldeh@un.edu.mx>
2026-01-28 11:58:54 -0600

    Saludo interactivo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" style="text-align: center; font-family: sans-serif; padding: 40px;">
+    <h1>Hola, {{ nombre || 'extraño' }}!</h1>
+    
+    <input 
+      v-model="nombre" 
+      placeholder="Escribe tu nombre..."
+      style="padding: 10px; font-size: 1.2rem;"
+    />
+
+    <p v-if="nombre.length > 5" style="color: green;">
+      ¡Qué nombre tan largo y elegante tienes!
+    </p>
+
+    <br><br>
+    <button @click="limpiarNombre">Borrar todo</button>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref } from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+// Definimos la variable reactiva
+const nombre = ref('')
+
+// Función para el botón
+const limpiarNombre = () => {
+  nombre.value = ''
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+button {
+  background-color: #42b983; /* El color verde oficial de Vue */
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  border-radius: 5px;
 }
 </style>
